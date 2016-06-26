97278ecf643bc5e9361937de0e4d23dea6708b1a
tomaszklos <tom.klos@gmail.com>
2016-06-26 21:36:40 +0200

    Add files via upload

---
+++ TomaszKlos_EQTechnicalExerciseTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TomaszKlos_EQTechnicalExerciseTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
